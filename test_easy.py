c5ca2ed0f65d6d290e380f85a35cea88c67f0098
rushighadmode@gmail.com <rushighadmode@gmail.com>
2024-03-21 13:18:31 +0000

    code change

--- test_easy.py
+++ test_easy.py
@@ -6,15 +6,10 @@ from pyspark.sql import SQLContext
 from pyspark.sql.types import StructType, StructField, StringType
 
 database_name = "default"
-table_name = "covid_data"
+table_name1 = "covid_data"
 
 # create a Spark session for you by default.
-@pytest.fixture(scope="session")
-def spark():
-    spark = SparkSession.builder \
-        .appName("pytest-pyspark") \
-        .master("local[2]") \
-        .getOrCreate()
+spark = SparkSession.builder.appName("Pytest-pyspark").getOrCreate()
 
 def test_table_existenece():
-    assert check_if_table_exists(table_name, database_name) is True, F"INVALID TABLE: The '{table_name}' table does not exist in the '{database_name}' database ..."
+    assert check_if_table_exists(table_name1, database_name) is True, F"INVALID TABLE: The '{table_name1}' table does not exist in the '{database_name}' database ..."
